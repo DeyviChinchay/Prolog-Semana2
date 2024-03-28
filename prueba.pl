c6336a43662a19c46803405d74588dde358ed220
LAB-USR-LSUR <LAB-USR-LSUR@grupoutp.net>
2024-03-27 21:47:51 -0500

    ultimo commit

--- prueba.pl
+++ prueba.pl
@@ -1,3 +1,17 @@
 
+/*HECHOS*/
+le_gusta_a(maria,jose).
+animal(perro).
+animal(gato).
+animal(canguro).
+arbol(palmera).
+flor(margarita).
+
+
 padre(juan,jose).
-padre(juan,maria).
+padre(juan,maria). 
+
+/*REGLAS*/
+
+vegetal(X):-arbol(X).
+vegetal(X):-flor(X).
